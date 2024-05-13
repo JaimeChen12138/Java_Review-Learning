40c1272cc3bff0a3752272679aa3544058e2cab5
yizhouchen <joe1542827496@gmail.com>
2024-05-13 14:05:22 -0700

    May13

--- JavaPattern/src/SingleTon1.java
+++ JavaPattern/src/SingleTon1.java
@@ -4,7 +4,7 @@ public class SingleTon1 {
   // 2) create the instance within the class
   // 3) create a public static method
   // 在类加载时创建 Singleton 实例。这种方式线程安全，
-  // 但即使不使用 Singleton 实例也会初始化。资源浪费
+  // 但即使不使用 Singleton 实例也会初始化。资源浪费/
   private static final SingleTon1 girlfriend = new SingleTon1("yujin");
 
   private String name;
